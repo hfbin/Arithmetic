e6229921f161abc670ab10a1b7073eccc8b7af35
hfbin <1716850375@qq.com>
2018-07-20 22:22:15 +0800

    Dynamic Array

--- src/cn/hfbin/array/DynamicArray.java
+++ src/cn/hfbin/array/DynamicArray.java
@@ -152,5 +152,28 @@ public class DynamicArray<E> {
             newData[i] = data[i];
         data = newData;
     }
+    public static void main(String[] args) {
+
+        DynamicArray arr = new DynamicArray(10);
+        for(int i = 0 ; i < 10 ; i ++)
+            arr.addLast(i);
+        System.out.println(arr);
+
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.addFirst(1000);
+        System.out.println(arr);
+        // [-1, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
+    }
 
 }
