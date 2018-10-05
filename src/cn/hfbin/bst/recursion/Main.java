43c815c318f86d4a9775736ba500c8ab931f34d9
hfbin <1716850375@qq.com>
2018-10-05 16:24:10 +0800

    bst recursion

---
+++ src/cn/hfbin/bst/recursion/Main.java
@@ -0,0 +1,30 @@
+package cn.hfbin.bst.recursion;
+
+import cn.hfbin.bst.recursion.BST;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for(int num: nums)
+            bst.add(num);
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        bst.preOrder();
+        System.out.println();
+
+        bst.inOrder();
+        System.out.println();
+
+        bst.postOrder();
+        System.out.println();
+    }
+}
--- src/cn/hfbin/linkedlist/dummylinkedlist/Main.java
+++ src/cn/hfbin/linkedlist/dummylinkedlist/Main.java
@@ -1,10 +1,13 @@
 package cn.hfbin.linkedlist.dummylinkedlist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
+       /* LinkedList<Integer> linkedList = new LinkedList<>();
         for(int i = 0 ; i < 5 ; i ++){
             linkedList.addFirst(i);
             System.out.println(linkedList);
@@ -20,6 +23,18 @@ public class Main {
         System.out.println(linkedList);
 
         linkedList.removeLast();
-        System.out.println(linkedList);
+        System.out.println(linkedList);*/
+
+        List<Integer> list = new ArrayList();
+        for(int i = 0 ; i < 5 ; i ++){
+            list.add(i);
+        }
+        System.out.println(list);
+        List<Integer>  list2 = list;
+        for(int i = 5 ; i < 10 ; i ++){
+            list2.add(i);
+        }
+        System.out.println("list2"+list2);
+        System.out.println("list"+list);
     }
 }
