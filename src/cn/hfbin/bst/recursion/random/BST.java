9a4eebb00e8abcf7d6823340ac9076ab3deee2ec
hfbin <1716850375@qq.com>
2018-10-19 15:27:47 +0800

    delete bst random number

--- src/cn/hfbin/bst/recursion/random/BST.java
+++ src/cn/hfbin/bst/recursion/random/BST.java
@@ -233,6 +233,62 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
+    //删除任意二叉树为e的节点
+    public void remove (E e){
+        
+        remove(root , e);
+    }
+
+    // 删除掉以node为根的二分搜索树中值为e的节点, 递归算法
+    // 返回删除节点后新的二分搜索树的根
+    Node remove(Node node, E e){
+
+        if( node == null )
+            return null;
+
+        if( e.compareTo(node.e) < 0 ){
+            node.left = remove(node.left , e);
+            return node;
+        }
+        else if(e.compareTo(node.e) > 0 ){
+            node.right = remove(node.right, e);
+            return node;
+        }
+        else{   // e.compareTo(node.e) == 0
+
+            // 待删除节点左子树为空的情况
+            if(node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            // 待删除节点左右子树均不为空的情况
+
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = new Node(minimum(node.right).e);
+            size ++;
+
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            size --;
+
+            return successor;
+        }
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
